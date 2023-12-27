671501219149416e9eeed4d03b97099ad0a6ff8f
myc <hallelu1513@gmail.com>
2023-12-27 12:49:05 +0900

    프로젝트를 생성하고 Main 클래스를 작성하였습니다.

---
+++ 231227-1/src/Main.java
@@ -0,0 +1,16 @@
+
+// VCS : versoin control system
+// svn, git
+
+// 다운로드 -> 환경변수 설정 -> CLI command 입력 제어
+
+// eclipse 내부에 git GUI 플러그인
+// 각자의 개인 컴퓨터에 자원(파일) 기록
+//local repository 생성
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+
+}
