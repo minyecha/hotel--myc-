aa81c259d99bde04f2ea37f7cdf2337c7b7af881
myc <hallelu1513@gmail.com>
2023-12-27 14:36:05 +0900

    add

--- 231227-1/src/Main.java
+++ 231227-1/src/Main.java
@@ -8,9 +8,16 @@
 // 각자의 개인 컴퓨터에 자원(파일) 기록
 //local repository 생성
 
+//commit -> 기록 생성
+//history -> 기록 열람
+//checkout -> 원하는 시점으로 이동
+
+//github -> remote repository
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("add");
+		
 	}
 
 }
